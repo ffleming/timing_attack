858a506a2106671b1536e554c4069d511041f78e
Forrest Fleming <ffleming@gmail.com>
2017-02-04 11:00:29 -0800

    move require to main file

--- lib/timing_attack.rb
+++ lib/timing_attack.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'ruby-progressbar'
 require "timing_attack/version"
 require "timing_attack/attacker"
+require 'timing_attack/spinner'
 require "timing_attack/brute_forcer"
 require "timing_attack/grouper"
 require "timing_attack/test_case"
